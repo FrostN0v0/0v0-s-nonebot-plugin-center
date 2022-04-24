9c6627515b1ef8b3cbf66fafcedc54e31e016082
FrostN0v0 <80870777+FrostN0v0@users.noreply.github.com>
2022-04-24 15:31:17 +0800

    支持获取服务器在线玩家ID

--- src/plugins/mcping/__init__.py
+++ src/plugins/mcping/__init__.py
@@ -1,5 +1,5 @@
 # Author: FrostN_0V0
-# Date  : 2022/4/22 22:01
+# Date  : 2022/4/24 22:01
 import base64
 from pathlib import Path
 from nonebot import on_command
@@ -11,7 +11,7 @@ import nonebot
 global_config = nonebot.get_driver().config
 nonebot.logger.info("global_config:{}".format(global_config))
 scheduler = require("nonebot_plugin_apscheduler").scheduler  # type:AsyncIOScheduler
-mcp = on_command("mcp", aliases=set(['查询服务器', 'ping']), priority=1)
+mcp = on_command("mc", aliases=set(['查询服务器', '#mc']), priority=1)
 @mcp.handle()
 async def ser(event:GroupMessageEvent):
     get_msg = event.get_plaintext()
@@ -25,10 +25,10 @@ async def ser(event:GroupMessageEvent):
 async def get_info(ip:str)->MessageSegment:
     server = JavaServer.lookup(ip)
     status = server.status()
+    #获取并处理服务器图标，缓存到本地发送
     a = status.favicon
     b = a.split(',')[1]
     img_data = base64.b64decode(b)
-    # print(img_data)
     with open('C:/sk/data/mcicon/icon.png', 'wb+') as f:
         f.write(img_data)
     max = status.raw['players']['max']
@@ -40,8 +40,6 @@ async def get_info(ip:str)->MessageSegment:
     vs = f"服务器版本：{version}\n"
     ping_msg = f"Ping:{ping}MS\n"
     server_icon = Path('C:/sk/data/mcicon/icon.png')
-
-
     msg = MessageSegment.image(server_icon)
     msg+= MessageSegment.text(player)
     msg+= MessageSegment.text(vs)
@@ -55,26 +53,42 @@ async def get_info(ip:str)->MessageSegment:
             for i in range(len(data)):
                 analyze_date(data[i], result + "[%s]" % i)
             else:
-                # print(result + "="+str(data))
                 remix = ''
                 for j in range(len(data)):
-                    # print(data[j]['text'])
                     real_data = data[j]['text']
                     remix = remix + real_data
-                # print(remix)
-                # print(type(remix))
                 ls.append(remix)
-                # print(ls)
                 return ls
     try:
         raw_dict = status.raw['description']['extra']
         data_return = analyze_date(raw_dict, result="motd")
-        color_motd = f"服务器信息:\n{data_return[0]}"
+        color_motd = f"服务器信息:\n{data_return[0]}\n"
         msg+= MessageSegment.text(color_motd)
-        # print(raw_dict)
     except:
         motd = status.raw['description']['text']
-        v_motd = f"服务器信息：\n{motd}"
+        v_motd = f"服务器信息：\n{motd}\n"
         msg+= MessageSegment.text(v_motd)
-        #print(v_motd)
+    #获取在线玩家列表
+    try:
+        try:
+            query=server.query()
+            query_name = query.players.names
+            pln = ''
+            for n in range(len(query_name)):
+                pln += f'{query_name[n]}\n'
+                pln_list = f"在线玩家：\n{pln}"
+            msg+=MessageSegment.text(pln_list)
+        except:
+            if status.players.sample is not None:
+                player_name = sorted([player.name for player in status.players.sample])
+            else:
+                player_name = []
+            pln = ''
+            for n in range(len(player_name)):
+                pln += f'{player_name[n]}\n'
+                pln_list = f"在线玩家：\n{pln}"
+            msg+=MessageSegment.text(pln_list)
+    except:
+            error="获取玩家列表失败"
+            print(error)
     return msg
